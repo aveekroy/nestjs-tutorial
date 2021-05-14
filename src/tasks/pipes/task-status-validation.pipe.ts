2b2a07da653b33abdc02ab59c0ce155f7d1dc151
Aveek Roy <aveek.roy@outlook.com>
2021-05-14 16:41:51 -0700

    Added Custom Pipes Validation

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,36 @@
+// Custom Pipe implementation
+
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    console.log('value', value);
+    console.log('metadata', metadata);
+
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(
+        'It is an invalid status, only OPEN, IN_PROGRESS, DONE is allowed',
+      );
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx != -1;
+  }
+}

5e23ac92c37f7087590bb8603736c8475b4679d7
Aveek Roy <aveek.roy@outlook.com>
2021-05-14 10:21:23 -0700

    Added Basic Pipes Validation

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -2,7 +2,12 @@
 // DTO - Data Transfer Object - an object that defines how the data will be sent across the network.
 // This gets passed to the controller, and then to service(provider) of the Tasks Module
 
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,8 @@ import {
   Patch,
   Post,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
@@ -33,6 +35,7 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksService.createTask(createTaskDto);
   }

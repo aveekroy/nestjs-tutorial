eb76a1e4b1e04508ffdbd4dcd2467407ecaec9f0
Aveek Roy <aveek.roy@outlook.com>
2021-05-18 07:01:28 -0700

    Initializing postgres db with TypeORM

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,3 +1,7 @@
+// The request - Query parameters that is coming is defined here in a DTO
+// DTO - Data Transfer Object - an object that defines how the data will be sent across the network.
+// This gets passed to the controller, and then to service(provider) of the Tasks Module
+
 import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../task.model';
 export class GetTasksFilterDto {

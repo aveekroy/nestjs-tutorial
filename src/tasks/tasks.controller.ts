f62c8b236cdf0bb10d68dd50c8eca2cb1fbc1f0a
Aveek Roy <aveek.roy@outlook.com>
2021-05-19 17:08:59 -0700

    Getting all the tasks with or without filters - Data Persistance

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -22,15 +22,12 @@ import { TaskStatus } from './task-status.enum';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  // @Get()
-  // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-  //   // Checking if any query parameter has been passed
-  //   if (Object.keys(filterDto).length) {
-  //     return this.tasksService.getTasksWithFilters(filterDto);
-  //   } else {
-  //     return this.tasksService.getAllTasks();
-  //   }
-  // }
+  @Get()
+  getTasks(
+    @Query(ValidationPipe) filterDto: GetTasksFilterDto,
+  ): Promise<Task[]> {
+    return this.tasksService.getTasks(filterDto);
+  }
 
   @Get('/:id')
   getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {

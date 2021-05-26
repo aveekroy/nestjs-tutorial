1ac5b5d1a8e9c273569d46fea289b26ef8b99cc2
Aveek Roy <aveek.roy@outlook.com>
2021-05-25 17:49:24 -0700

    Sign in and validating password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,4 +13,11 @@ export class AuthService {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
   }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto) {
+    const result = await this.userRepository.validateUserPassword(
+      authCredentialsDto,
+    );
+    console.log(result);
+  }
 }
